dceaa77c0edfc325fd079fab75ce7cae9dbdfb00
tatiana <tatyana.sergienko.95@mail.ru>
2021-11-23 15:45:53 +0300

    refactoring MapsFragment

--- app/src/main/java/com/example/photosdemo/ui/fragments/maps/MapsFragment.kt
+++ app/src/main/java/com/example/photosdemo/ui/fragments/maps/MapsFragment.kt
@@ -1,13 +1,18 @@
 package com.example.photosdemo.ui.fragments.maps
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import coil.ImageLoader
+import coil.request.ImageRequest
+import coil.request.SuccessResult
 import com.example.photosdemo.R
-import com.example.photosdemo.data.models.image.ImageDtoOut
 import com.example.photosdemo.data.remote.SessionManager
 import com.example.photosdemo.databinding.FragmentMapsBinding
 import com.example.photosdemo.viewmodel.PhotosViewModel
@@ -15,6 +20,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
+import kotlinx.coroutines.launch
 
 class MapsFragment : Fragment(), OnMapReadyCallback  {
     private var _binding: FragmentMapsBinding? = null
@@ -22,7 +28,7 @@ class MapsFragment : Fragment(), OnMapReadyCallback  {
     private lateinit var googleMap: GoogleMap
     private lateinit var viewModel: PhotosViewModel
     private lateinit var sessionManager: SessionManager
-    private val photos = mutableListOf<ImageDtoOut?>()
+    private var bmp: Bitmap? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,22 +46,39 @@ class MapsFragment : Fragment(), OnMapReadyCallback  {
         sessionManager = SessionManager(requireContext())
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(this)
-
-        viewModel.getImages(sessionManager.fetchAuthToken()!!).observe(viewLifecycleOwner, {
-            photos.clear()
-            if (it != null) {
-                it.data?.let { list -> photos.addAll(list) }
-            }
-        })
     }
 
     override fun onMapReady(gMap: GoogleMap) {
         googleMap = gMap
 
-        for (i in photos) {
-            val latLng = LatLng(i?.lat!!, i.lng!!)
-            googleMap.addMarker(MarkerOptions().position(latLng))
-        }
+        viewModel.getImages(sessionManager.fetchAuthToken()!!).observe(viewLifecycleOwner, {
+            if (it.data != null) {
+                for (i in it.data) {
+                    val latLng = LatLng(i.lat!!, i.lng!!)
+                    val marker = googleMap.addMarker(MarkerOptions().position(latLng))
+                    lifecycleScope.launch {
+                        bmp = i.url?.let { url ->
+                            getBitmap(url)
+                        }
+                    }
+                    val finalBmp = bmp?.let {
+                            bitmap -> Bitmap.createScaledBitmap(bitmap, 100, 100, false)
+                    }
+                    marker?.setIcon(finalBmp?.let { bitmap ->
+                        BitmapDescriptorFactory.fromBitmap(bitmap)
+                    })
+                }
+            }
+        })
+    }
+
+    private suspend fun getBitmap(url: String) : Bitmap? {
+        val loading = ImageLoader(requireContext())
+        val request = ImageRequest.Builder(requireContext())
+            .data(url)
+            .build()
+        val result = (loading.execute(request) as SuccessResult).drawable
+        return (result as BitmapDrawable).bitmap
     }
 
     override fun onDestroyView() {
